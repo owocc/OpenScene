c7b49392c0e778f6deaf4915bba2185d19d5fb3d
owocc <owocc@qq.com>
2026-08-21 18:28:58 +0800

    chore(studio): install shadcn button group

--- apps/studio/src/components/ui/separator.tsx
+++ apps/studio/src/components/ui/separator.tsx
@@ -1,20 +1,14 @@
 import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
-import type { ComponentProps } from "react";
 
 import { cn } from "@/lib/utils";
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  ...props
-}: ComponentProps<typeof SeparatorPrimitive>) {
+function Separator({ className, orientation = "horizontal", ...props }: SeparatorPrimitive.Props) {
   return (
     <SeparatorPrimitive
       data-slot="separator"
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+        "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className,
       )}
       {...props}
